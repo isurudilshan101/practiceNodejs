42561b9db73b6252a06daa0857eeef6d0e8f1986
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-02-25 17:52:44 +0530

    project1

--- nodejs1/server.js
+++ nodejs1/server.js
@@ -1,6 +1,6 @@
 var http=require('http');
 http.createServer(function(req,res){
-    res.end("helcvxvxbcbvcxvsdflo world");
+    res.end("helcvxwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwvxbcbvcxvsdflo world");
  
 }).listen(8080); 
 console.log("nodejs server is listning");
