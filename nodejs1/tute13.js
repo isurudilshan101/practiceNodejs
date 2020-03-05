8f0510e1c842f25cf336f4047c403df7f126ebba
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-03-05 16:00:57 +0530

    handlebars2

--- nodejs1/tute13.js
+++ nodejs1/tute13.js
@@ -2,17 +2,21 @@ var express=require('express');
 var app=express();
 var mysql=require('mysql'); 
 var handlebars=require('express-handlebars');
-
-
 var con;
+app.engine('handlebars',handlebars({defaultLayout:'main'}));
+app.set('view engine','handlebars');
 app.get('/',function(req,res){
-   
     
-    });
-
+    
     con.query("SELECT * FROM student1",function(err,result){
         if(err) throw err;
         console.log(result);
+        res.render('student',{
+            results:result
+        });
+    
+    });
+
 
 
 });
