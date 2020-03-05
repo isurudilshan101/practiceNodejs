4ca52c3c59900634da5a48af068f3e77fa5f3da6
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-03-05 08:39:27 +0530

    mysql

---
+++ nodejs1/tute12.js
@@ -0,0 +1,9 @@
+var express=require('express');
+car app=express();
+var mysql=require('mysql'); 
+app.get('/',function(req,res){
+
+});
+app.listen(4000,function(){
+    console.log('listing to prt 3000');
+});
